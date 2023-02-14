87bb69758fd1373009ca1867b303689cef406fd7
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-02-14 14:44:43 -0800

    condition to send the email

--- Class01-Introduce/index.js
+++ Class01-Introduce/index.js
@@ -76,19 +76,39 @@ function favouriteFruit(theme) {
 
 function sendEmail(){
     var emailPerson = document.getElementById("textinput").value;
-    
-    console.log(emailPerson)
 
-    Email.send({
-        Host:      "smtp.gmail.com",
-        Username : "<sender’s email address>",
-        Password : "<email password>",
-        To :       "<recipient’s email address>",
-        From :     `${emailPerson}`,
-        Subject :  "<email subject>",
-        Body :     "<email body>",
-    })
-    .then(function(message){
-        alert("mail sent successfully")
-    })
+    let checkemail = checkValidateEmail(emailPerson)
+
+    if (checkemail === true){
+        Email.send({
+            Host:      "smtp.gmail.com",
+            Username : "<sender’s email address>",
+            Password : "<email password>",
+            To :       "<recipient’s email address>",
+            From :     `${emailPerson}`,
+            Subject :  "<email subject>",
+            Body :     "<email body>",
+        }).then(function(message)
+        {
+            alert("mail sent successfully")
+            document.getElementById("textinput").value = null
+        })
+    }
+    else
+    {
+        alert("Invalid email");
+    }
+}
+
+function checkValidateEmail(emailPerson){
+
+    let check = /^[\w.\+]+@\w+.\w{2,}(?:.\w{2})?$/
+    
+    if (check.test(emailPerson))
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
